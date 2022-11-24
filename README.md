ec6d88349ac2ebe7a68f19cc59f61b448b9124fe
Webbb3 <m12.vovk@gmail.com>
2022-11-24 03:30:04 +0200

    animated drop menu

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# web
-my study on my owm
+# Web development
 
+my study on my owm :milky_way:	
+
+https://roadmap.sh/frontend - this is a frontend **roadmap** (it looks very scary at first glance, but in fact it's not so scary) - you can only learn from it.
+
+https://javascript.info - The modern **JavaScript** tutorial. 
+(there is also a translation in Ukrainian https://uk.javascript.info/)
+
+:video_game: And I would like to add some interesting games for **CSS** learning:
+- https://codepip.com/games/grid-garden/
+- https://codepip.com/games/flexbox-froggy/
+- https://flukeout.github.io
+
+	
---
+++ drop-down-menu/js/script.js
@@ -0,0 +1,26 @@
+let isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
+		let body=document.querySelector('body');
+if(isMobile.any()){
+		body.classList.add('touch');
+		let arrow=document.querySelectorAll('.arrow');
+	for(i=0; i<arrow.length; i++){
+			let thisLink=arrow[i].previousElementSibling;
+			let subMenu=arrow[i].nextElementSibling;
+			let thisArrow=arrow[i];
+
+			thisLink.classList.add('parent');
+		arrow[i].addEventListener('click', function(){
+			subMenu.classList.toggle('open');
+			thisArrow.classList.toggle('active');
+		});
+	}
+}else{
+	body.classList.add('mouse');
+}
